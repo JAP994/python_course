1df07b67ace6f05b17a26b65c519af683a220699
“JAP994” <suco@MacBook-Air-de-Jonathan.local>
2026-04-13 13:35:37 -0500

    variables en python

--- 01_variables.py
+++ 01_variables.py
@@ -3,4 +3,35 @@
 my_string_variable = "My string variables"
 print(my_string_variable)
 
-my_int_variable= 5
+my_int_variable = 5
+my_bool_variable = True
+print(my_bool_variable, my_string_variable, my_int_variable, "concatenacion")
+# ALGUNAS FUNCIONES DEL SISTEMA
+# len cuenta el numero de caracteres de cada string contando
+# los espacios
+print(len(my_string_variable))
+
+first_name = 'Asabeneh' 
+# Printing out types
+print(type('Asabeneh'))          # str
+print(type(first_name))          # str
+print(type(10))                  # int
+print(type(3.14))                # float
+print(type(1 + 1j))              # complex
+print(type(True))                # bool
+print(type([1, 2, 3, 4]))        # list
+print(type({'name':'Asabeneh'})) # dict
+print(type((1,2)))               # tuple
+print(type(zip([1,2],[3,4])))    # zip
+second_name = input('What is your name:')
+age = input('Wow old are you:')
+print("nombre; ", second_name, "edad:",  age) 
+
+# De esta forma forzmos el tipo de dato
+address: str = 'Mi direccion'
+address = 32
+print(address)
+
+
+
+
