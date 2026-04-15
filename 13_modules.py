d988f4c0eed2df2258d1a7844b549eec2361a7dd
“JAP994” <suco@MacBook-Air-de-Jonathan.local>
2026-04-15 10:41:33 -0500

    python bases

---
+++ 13_modules.py
@@ -0,0 +1,10 @@
+# MODULES #
+from my_module import *
+
+sum(1,2,3)
+printValues("Hola Python")
+
+import math
+
+print(math.pi)
+print(math.pow(2 , 8))
---
+++ reto01.py
@@ -0,0 +1,14 @@
+# RESTO DE PROGRAMACION
+# https://docs.python.org/es/3.14/tutorial/index.html
+
+# COMENTARIO SIMPLE
+"""""
+COMENTARIOS EN ARIAL L
+
+HASTA CUMPLIT EL BLOQUE DE TEXTO
+"""""
+# REPRESENTACION DE UNA VARIABLE SUS TIPOS DE DATOS
+luna = "Luna string"
+luna = 32
+luna = 32.03
+luna = True
